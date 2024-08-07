899e0918aae68936d6fc80a505d426734c0e644a
Kevin <kevin@twjoin.com>
2024-08-07 13:30:20 +0800

    create project

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by 陳冠甫 on 2024/8/7.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
